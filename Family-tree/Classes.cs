a04aee0b19a2dfa6edcfddea8e51dcfc9f68f207
Кожеуров Денис < >
2016-04-22 10:11:19 +0300

    Работа с csv-файлом

--- Family-tree/Classes.cs
+++ Family-tree/Classes.cs
@@ -1,93 +1,124 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
-using System.Text;
 
-/*Класс "Отношение" определяет отношение между двумя людьми:
- * 1.Время совместной жизни
- * 2.ID Детей*/
-public class Relation
+namespace Objects
 {
-    public int ID { get; set; }
-    public int YEAR1 { get; set; }
-    public int YEAR2 { get; set; }
-    public List<int> Childs { get; set; }
-    public static int MaxID { get; set; }//Данная переменная ннеобходима для создания новой связи
-
-    public Relation(int id, int year1, int year2)
+    /*Класс "Отношение" определяет отношение между двумя людьми:
+     * 1.Время совместной жизни
+     * 2.ID Детей*/
+    public class Relation
     {
-        ID = id;
-        if (MaxID < ID)
-            MaxID = ID;
-        YEAR1 = year1;
-        YEAR2 = year2;
-        Childs = new List<int>();
+        public int ID;
+        public int YEAR1;
+        public int YEAR2;
+        public List<int> Childs;
+        public static int MaxID;//Данная переменная ннеобходима для создания новой связи
+
+        public Relation(int id, int year1, int year2)
+        {
+            ID = id;
+            if (MaxID < ID)
+                MaxID = ID;
+            YEAR1 = year1;
+            YEAR2 = year2;
+            Childs = new List<int>();
+        }
+
+        public void AddChild(int CID)
+        {
+            Childs.Add(CID);
+        }
     }
 
-    void AddChild(int CID)
+    //Класс "Человек" содержит данные о человеке
+    public class Man
     {
-        Childs.add(CID);
+        public int ID;
+        public string Name;
+        public string Family;
+        public string PatName;
+        public string Sex;
+        public DateTime Birthday;
+        public DateTime DeathDate;
+        public List<Relation> Relations;
+
+        private static int MaxID;//Данная переменная ннеобходима для создания нового человека
+        private bool isDead;
+
+        //Создание класса при загрузке из БД
+        public Man(int id, string name, string family, string patname, string sex, string birthday, string deathdate)
+        {
+            ID = id;
+            if (MaxID < ID)
+                MaxID = ID;
+            Name = name;
+            Family = family;
+            PatName = patname;
+            Sex = sex;
+            Relations = new List<Relation>();
+            DateTime.TryParseExact(birthday, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out Birthday);
+            if (deathdate != null)
+            {
+                DateTime.TryParseExact(deathdate, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out DeathDate);
+                isDead = true;
+            }
+            else
+                isDead = false;
+        }
+
+        //Создание нового человека
+        public Man(string name, string family, string patname, string sex, string birthday, string deathdate)
+        {
+            ID = MaxID;
+            MaxID++;
+            Name = name;
+            Family = family;
+            PatName = patname;
+            Sex = sex;
+            Relations = new List<Relation>();
+            DateTime.TryParseExact(birthday, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out Birthday);
+            if (deathdate != null)
+            {
+                DateTime.TryParseExact(deathdate, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out DeathDate);
+                isDead = true;
+            }
+            else
+                isDead = false;
+        }
+
+        public Man(string csvLine, char div)
+        {
+            string[] parts = csvLine.Split(div);
+            ID = Int32.Parse(parts[0]);
+            if (MaxID < ID)
+                MaxID = ID;
+            Name = parts[1];
+            Family = parts[2];
+            PatName = parts[3];
+            Sex = parts[4];
+            Relations = new List<Relation>();
+            DateTime.TryParseExact(parts[5], "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out Birthday);
+            if (parts[6] != null)
+            {
+                DateTime.TryParseExact(parts[6], "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out DeathDate);
+                isDead = true;
+            }
+            else
+                isDead = false;
+        }
+
+        //Добавление отношений из БД
+        void AddRelation(int ID, int year1, int year2)
+        {
+            Relations.Add(new Relation(ID, year1, year2));
+        }
+        //Создание нового связи между людьми
+        static public void AddNewRelation(Man Man1, Man Man2, int year1, int year2)
+        {
+            int ID = Man.MaxID + 1;
+            Man1.AddRelation(ID, year1, year2);
+            Man2.AddRelation(ID, year1, year2);
+        }
     }
-}
-
-//Класс "Человек" содержит данные о человеке
-public class Man
-{
-    public int ID { get; set; }
-    public string Name { get; set; }
-    public string Family { get; set; }
-    public string PatName { get; set; }
-    public string Sex { get; set; }
-    public DateTime Birthday { get; set; }
-    public DateTime DeathDate { get; set; }
-    public List<int> Relations { get; set; }
-    private static int MaxID { get; set; }//Данная переменная ннеобходима для создания нового человека
-
-    //Создание класса при загрузке из БД
-    public Man(int id, string name, string family, string patname, string sex, string birthday, string deathdate)
-    {
-        ID = id;
-        if (MaxID < ID)
-            MaxID = ID;
-        Name = name;
-        Family = family;
-        PatName = patname;
-        Sex = sex;
-        Relations = new List<int>();
-        DateTime.TryParseExact(birthday, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out Birthday);
-        if (deathdate != null)
-            DateTime.TryParseExact(deathdate, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out DeathDate);
-        else
-            DeathDate = null;
-    }
-
-    //Создание нового человека
-    public Man(string name, string family, string patname, string sex, string birthday)
-    {
-        ID = MaxID;
-        MaxID++;
-        Name = name;
-        Family = family;
-        PatName = patname;
-        Sex = sex;
-        Relations = new List<int>();
-        DateTime.TryParseExact(birthday, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out Birthday);
-        if (deathdate != null)
-            DateTime.TryParseExact(deathdate, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out DeathDate);
-        else
-            DeathDate = null;
-    }
-
-    //Добавление отношений из БД
-    void AddRelation(int ID, int year1, int year2)
-    {
-        Relations.add(new Relation(ID, year1, year2));
-    }
-}
-
-//Создание нового связи между людьми
-void AddNewRelation(Man Man1, Man Man2, int year1, int year2)
-{
-    int ID = Man.MaxID + 1;
-    Man1.AddRelation(ID, year1, year2);
-    Man2.AddRelation(ID, year1, year2);
 }
---
+++ Family-tree/Using_csv.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csv
+{
+    public class csv
+    {
+        public string path;
+        public char div;
+   
+        public csv(string PATH)
+        {
+            div = ';';
+            path = PATH;
+            try
+            {
+                StreamReader sr = new StreamReader(path);
+                sr.Close();
+            }
+            catch(FileNotFoundException)
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                string line = "ID;Имя,Фамилия;Отчество;Пол;День рождения;День смерти";
+                byte[] encodedBytes = utf8.GetBytes(line);
+                File.WriteAllBytes(path, encodedBytes);
+            }
+        }
+
+        public csv(string PATH, char DIV)
+        {
+            div = DIV;
+            path = PATH;
+        }
+        
+        public List<string> Reading()
+        {
+            List<string> result = new List<string>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                sr.ReadLine();
+                while ((line =sr.ReadLine())!=null)
+                {
+                    result.Add(line);
+                }
+            }
+            return result;
+        }
+
+        public void Writing (string line)
+        {
+            FileStream fs = File.OpenWrite(path);
+            fs.Lock(1, 1);
+            fs.Position=fs.Length;
+            UTF8Encoding utf8 = new UTF8Encoding();
+            byte[] encodedBytes = utf8.GetBytes(line);
+            fs.Write(encodedBytes, 0, line.Length);
+            fs.Close();
+        }
+    }
+}
+
+namespace Program
+{
+    public class Program
+    {
+        static public int Main()
+        {
+            csv.csv File = new csv.csv("C:/Users/admin/Documents/Family-tree/Family-tree/test.csv");
+            File.Writing("1,2,3,4,5");
+            return 1;
+        }
+    }
+}
