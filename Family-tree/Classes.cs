01e5042828ec76c6489412a1bec01d23aac44c1c
Кожеуров Денис < >
2016-04-22 10:31:01 +0300

    Добавление класса Дети

--- Family-tree/Classes.cs
+++ Family-tree/Classes.cs
@@ -5,29 +5,61 @@ using System.Collections.Generic;
 namespace Objects
 {
     /*Класс "Отношение" определяет отношение между двумя людьми:
-     * 1.Время совместной жизни
-     * 2.ID Детей*/
+     * 1.Время совместной жизни*/
     public class Relation
     {
         public int ID;
+        public int Man1ID;
+        public int Man2ID;
         public int YEAR1;
         public int YEAR2;
-        public List<int> Childs;
         public static int MaxID;//Данная переменная ннеобходима для создания новой связи
 
-        public Relation(int id, int year1, int year2)
+        public Relation(string csvLine, char div)//int id, int man1ID, int man2ID, int year1, int year2)
         {
-            ID = id;
+            string[] parts = csvLine.Split(div);
+            ID = Int32.Parse(parts[0]);
             if (MaxID < ID)
                 MaxID = ID;
-            YEAR1 = year1;
-            YEAR2 = year2;
-            Childs = new List<int>();
+            Man1ID = Int32.Parse(parts[1]);
+            Man2ID = Int32.Parse(parts[2]);
+            YEAR1 = Int32.Parse(parts[3]);
+            YEAR2 = Int32.Parse(parts[4]);
         }
 
-        public void AddChild(int CID)
+        public Relation(Man man1, Man man2, int year1, int year2)
         {
-            Childs.Add(CID);
+            Man1ID = man1.ID;
+            Man2ID = man2.ID;
+            ID = MaxID + 1;
+            MaxID++;
+            YEAR1 = year1;
+            YEAR2 = year2;
+        }
+
+        public string ToLine()
+        {
+            string result= string.Concat(ID.ToString(),";",Man1ID.ToString(),";", Man2ID.ToString(),";", YEAR1.ToString(),";", YEAR2.ToString());
+            return result;
+        }
+    }
+
+    public class Childs
+    {
+        public int ID;
+        public int ChildID;
+
+        public Childs(string csvLine, char div)
+        {
+            string[] parts = csvLine.Split(div);
+            ID = Int32.Parse(parts[0]);
+            ChildID = Int32.Parse(parts[1]);
+        }
+
+        public Childs(int id, int childid)
+        {
+            ID = id;
+            ChildID = childid;
         }
     }
 
@@ -41,7 +73,6 @@ namespace Objects
         public string Sex;
         public DateTime Birthday;
         public DateTime DeathDate;
-        public List<Relation> Relations;
 
         private static int MaxID;//Данная переменная ннеобходима для создания нового человека
         private bool isDead;
@@ -56,7 +87,6 @@ namespace Objects
             Family = family;
             PatName = patname;
             Sex = sex;
-            Relations = new List<Relation>();
             DateTime.TryParseExact(birthday, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out Birthday);
             if (deathdate != null)
             {
@@ -76,7 +106,6 @@ namespace Objects
             Family = family;
             PatName = patname;
             Sex = sex;
-            Relations = new List<Relation>();
             DateTime.TryParseExact(birthday, "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out Birthday);
             if (deathdate != null)
             {
@@ -97,7 +126,6 @@ namespace Objects
             Family = parts[2];
             PatName = parts[3];
             Sex = parts[4];
-            Relations = new List<Relation>();
             DateTime.TryParseExact(parts[5], "dd.MM.yyyy HHmmss", null, DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AdjustToUniversal, out Birthday);
             if (parts[6] != null)
             {
@@ -106,19 +134,6 @@ namespace Objects
             }
             else
                 isDead = false;
-        }
-
-        //Добавление отношений из БД
-        void AddRelation(int ID, int year1, int year2)
-        {
-            Relations.Add(new Relation(ID, year1, year2));
-        }
-        //Создание нового связи между людьми
-        static public void AddNewRelation(Man Man1, Man Man2, int year1, int year2)
-        {
-            int ID = Man.MaxID + 1;
-            Man1.AddRelation(ID, year1, year2);
-            Man2.AddRelation(ID, year1, year2);
         }
     }
 }
